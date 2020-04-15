c29b0f408d8aa4750723cfc1efe797404dc334c6
Rafael Telles <rafael.sanches@jaya.tech>
2020-04-15 15:17:35 -0400

    motve to folder libs

---
+++ src/libs/DevelopersPortal.groovy
@@ -0,0 +1,28 @@
+#!groovy
+package libs;
+
+void commitFilesDevelopersPortal(service,
+                                 gitUrl,
+                                 gitCommitEmail,
+                                 gitCommitUsername,
+                                 pathSources,
+                                 folderRepository = 'developers-portal') {
+    def pathMetada = "src/data/metadata/"
+    sh "git clone $gitUrl $folderRepository"
+    sh "mkdir -p $folderRepository/$pathMetada"
+    sh "cp $pathSources $folderRepository/$pathMetada"
+    sh "git config --global user.email '$gitCommitEmail'"
+    sh "git config --global user.name '$gitCommitUsername'"
+    sh "cd $folderRepository && git add ."
+
+    def exist = sh(returnStdout: true, script: "cd $folderRepository && git status -s")
+
+    if (!"".equals(exist)) {
+        sh "cd $folderRepository && git commit -m 'jenkins build $service ${env.BUILD_NUMBER}'"
+        sh "cd $folderRepository && git push"
+    } else {
+        echo "No changes found. Skipping git commit."
+    }
+}
+
+return this
